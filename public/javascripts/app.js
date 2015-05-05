f1b3bb25f887f632cda84405affe82daaf6cf7ce
Ariel <borochov@american.edu>
2015-05-05 00:13:36 -0400

    Initial commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by arielb on 5/4/2015.
+ */
+angular.module('myApp', []).controller ('appCtrl',function ($scope){
+    $scope.firstname="Ariel";
+    $scope.lastname="borochov";
+});
